b7fb4daf15f6777c9a8c804f27f579f3a933058f
Sheikh Billah (Alif) <ambam3110@gmail.com>
2020-10-30 09:22:59 -0700

    Push 3: Dynamic sampling filtering done

--- npz_to_text.py
+++ npz_to_text.py
@@ -1,23 +1,16 @@
 import numpy as np
 import json
 
-data = np.load("event998.npz", allow_pickle=True)
-for x in data:
-    print(x)
+npz_file = "event998.npz"
+JSON_file = "npz.JSON"
+data = np.load(npz_file, allow_pickle=True)
+file = open(JSON_file, 'w')
 
-# print(data.files)
-# row = data.files
-# np.set_printoptions(threshold=np.inf)
-# print(data['root_file'])
-# sys.stdout=open("test.txt","w")
-# for i in row:
-#     print("--------------------------")
-#     print(data[i])
-# sys.stdout.close()
-
-# import numpy as np
-# import h5py
-# filename = 'multimuonfile.h5'
-# f = h5py.File(filename, 'r')
-# np.savetxt('h5file.txt', f['dataset'][...])
-# f.close()
+i = 0
+while i < 3000:
+    file.write("{ \n")
+    for x in data:
+        file.write(f"    '{x}': {data[x][i]}\n")
+    file.write("}\n")
+    i += 1
+file.close()
